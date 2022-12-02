fc6abb0d353d6e466780cd9d3b7fe7662658d447
Ian Shakespeare <ishakespeare@wilsonelectronics.com>
2022-12-02 11:08:27 -0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+<div class="w-full h-full bg-neutral-800 text-white">
+    <h1 class="text-3xl">Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
